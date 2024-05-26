f9e9e71a2bffdca72e2338ae4a02d27cdb22fd6a
n2tx8mds <git.antibody876@passinbox.com>
2024-05-26 22:17:16 +0200

    🔧 Set-up ESLint.
    Setted-up ESLint with Vue Eslint plugin & configured ESLint for Vue.

--- src/main.js
+++ src/main.js
@@ -4,3 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const xyz = 123
+
+function exemple(arg) {
+  if (arg == 1) {
+    //do something
+  } else if (arg == 2) {
+    // do something
+  } else if (arg == 1) {
+    // do something
+  }
+}
+
+exemple(xyz)
