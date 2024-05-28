9d8955561df9000bed96e54664519f033b6ef3f6
n2tx8mds <git.antibody876@passinbox.com>
2024-05-28 18:32:04 +0200

    🔧 Set up tailwindcss.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
 import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-const xyz = 123
-
-function exemple(arg) {
-  if (arg == 1) {
-    //do something
-  } else if (arg == 2) {
-    // do something
-  } else if (arg == 1) {
-    // do something
-  }
-}
-
-exemple(xyz)
